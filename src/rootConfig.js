d98c30758c5e9d0294e3516877cb317ed5386eda
vystii <ngahabanda@gmail.com>
2023-08-17 12:34:32 +0100

    rootConfig

--- src/rootConfig.js
+++ src/rootConfig.js
@@ -2,11 +2,8 @@ import RequestBasicAuthen from 'drupal-vuejs/src/App/BasicAuthentification/Reque
 export default {
   ...RequestBasicAuthen,
   languageId:
-    window.drupalSettings &&
-      window.drupalSettings.path &&
-      window.drupalSettings.path.pathPrefix
+    window.drupalSettings && window.drupalSettings.path && window.drupalSettings.path.pathPrefix
       ? window.drupalSettings.path.pathPrefix
       : null,
-  TestDomain: 'http://timer.vst',
   debug: true
 }
